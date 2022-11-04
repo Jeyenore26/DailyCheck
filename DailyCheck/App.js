2f82a55eb6f5ce38fa4da10d2b4c062ba18dcb2a
Jeyenore <78587875+Jeyenore26@users.noreply.github.com>
2022-11-04 18:17:31 +0200

    AMAKKU GAY

--- DailyCheck/App.js
+++ DailyCheck/App.js
@@ -1,25 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 import { TailwindProvider } from 'tailwindcss-react-native';
+import AppLoading from 'expo-app-loading';
 
+import {
+  useFonts,
+  Krub_200ExtraLight,
+  Krub_200ExtraLight_Italic,
+  Krub_300Light,
+  Krub_300Light_Italic,
+  Krub_400Regular,
+  Krub_400Regular_Italic,
+  Krub_500Medium,
+  Krub_500Medium_Italic,
+  Krub_600SemiBold,
+  Krub_600SemiBold_Italic,
+  Krub_700Bold,
+  Krub_700Bold_Italic,
+} from '@expo-google-fonts/krub';
 export default function App() {
-  return (
-    <TailwindProvider>
-    <View className="h-20 relative bg-green-600">
-       <View className="shadow-md fixed w-screen flex items-center bg-base h-20 z-80">
-         <View className="container w-full flex justify-between items-center">
-           <View className="flex items-center gap-2">
-             <View className = 'mt-8'>
-             <Text className = 'text-white  mt-8'>
-                 Earth
-               </Text>
+  let [fontsLoaded] = useFonts({
+    Krub_200ExtraLight,
+    Krub_200ExtraLight_Italic,
+    Krub_300Light,
+    Krub_300Light_Italic,
+    Krub_400Regular,
+    Krub_400Regular_Italic,
+    Krub_500Medium,
+    Krub_500Medium_Italic,
+    Krub_600SemiBold,
+    Krub_600SemiBold_Italic,
+    Krub_700Bold,
+    Krub_700Bold_Italic,
+  });
+  
+  let fontSize = 10;
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <TailwindProvider>
+      <View className="h-20 relative bg-green-600">
+         <View className="shadow-md fixed w-screen flex items-center bg-base h-20 z-80">
+           <View className="container w-full flex justify-between items-center">
+             <View className="flex items-center gap-2">
+               <View className = 'mt-8'>
+                    <Text className= 'mt-12' style={{
+              fontSize,
+              
+              // Note the quoting of the value for `fontFamily` here; it expects a string!
+              fontFamily: 'Krub_700Bold_Italic',
+            }}>
+                      AMAKKU IS SO GAY
+                    </Text>
+               </View>
              </View>
            </View>
          </View>
-       </View>
-      </View>
-  </TailwindProvider>
-  );
+        </View>
+    </TailwindProvider>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
